78844e0e9d321b81f40f0ea18c883b6629f4a212
arkad <mojib.khabireh@gmail.com>
2022-03-31 16:11:44 +0430

    exception for PatientAppt

--- src/main/java/App/Mani.java
+++ src/main/java/App/Mani.java
@@ -2,17 +2,7 @@ package App;
 
 public class Mani {
     public static void main(String[] args) {
-       /* PatientApp patientApp = new PatientApp();
-        patientApp.singUp();*/
-/*        SecretaryApp secretaryApp = new SecretaryApp();
-        secretaryApp.addClinic();
-        secretaryApp.addDoctor();*/
-//        PatientApp patientApp = new PatientApp();
-//        patientApp.setIdP(2);
-//        patientApp.myVisit();
-        DoctorApp doctorApp = new DoctorApp();
-        doctorApp.setId(1);
-        doctorApp.seePrescription();
+
 
     }
 }
--- src/main/java/App/PatientApp.java
+++ src/main/java/App/PatientApp.java
@@ -5,62 +5,98 @@ import Entity.Doctor;
 import Entity.Patient;
 import Entity.Visit;
 import service.imp.PatientService;
+
 import java.util.List;
 import java.util.Scanner;
 
 public class PatientApp {
-   private Integer idP;
+    private Integer idP;
     PatientService patientService = new PatientService();
     Scanner scanner = new Scanner(System.in);
-    public void singUp(){
-        Patient patient= new Patient();
-        System.out.println("please insert name");
-        String name= scanner.next().trim();
-        patient.setName(name);
-        patientService.add(patient);
-        //exception illegal insert
-    }
-    public void seeAllDoctor(){
-       List<Doctor> list= patientService.allDoctor();
-        if (list!=null){
-            list.forEach(System.out::println);
+
+    public void singUp() {
+        try {
+
+            Patient patient = new Patient();
+            System.out.println("please insert name");
+            String name = scanner.next().trim();
+            patient.setName(name);
+            patientService.add(patient);
+            //exception illegal insert
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-    public void seeAllClinic(){
-        List<Clinic> list= patientService.allClink();
-        if (list!=null){
-            list.forEach(System.out::println);
+
+    public void seeAllDoctor() {
+        try {
+            List<Doctor> list = patientService.allDoctor();
+
+            if (list != null) {
+                list.forEach(System.out::println);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-    private Doctor setDoctor(Integer id){
-        Doctor doctor=null;
-        doctor = patientService.findById(id);
-        return doctor;
+
+    public void seeAllClinic() {
+        try {
+            List<Clinic> list = patientService.allClink();
+            if (list != null) {
+                list.forEach(System.out::println);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    public void getTime(int ids){
+
+    private Doctor setDoctor(Integer id) {
+        try {
+            Doctor doctor = null;
+            doctor = patientService.findById(id);
+            return doctor;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void getTime(int ids) {
         Doctor doctor = setDoctor(ids);
         patientService.changeTime(doctor);
         Patient patient = findByIdP();
-        if (doctor!=null){
-            Visit visit = new Visit(doctor,patient,doctor.getStartWork().minusMinutes(15L));
+        if (doctor != null) {
+            Visit visit = new Visit(doctor, patient, doctor.getStartWork().minusMinutes(15L));
             patientService.giveTime(visit);
 
-        }else System.out.println("can't");
+        } else System.out.println("can't");
     }
-    public Patient findByIdP(){
+
+    public Patient findByIdP() {
         Patient patient = patientService.findByIdP(this.idP);
         return patient;
     }
+
     public void setIdP(Integer idP) {
         this.idP = idP;
     }
-    public void myVisit(){
-        List<Visit> list= patientService.myVisit(this.idP);
-        if (list!=null){
-            list.forEach(System.out::println);
+
+    public void myVisit() {
+        try {
+            List<Visit> list = patientService.myVisit(this.idP);
+            if (list != null) {
+                list.forEach(System.out::println);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-    public void cancelVisit(){}
-    public void doctorOfClinic(Integer id){}
+
+    public void cancelVisit() {
+    }
+
+    public void doctorOfClinic(Integer id) {
+    }
 
 }
