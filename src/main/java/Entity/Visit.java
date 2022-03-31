5f9d4e8dee269ba25b4db03a9a3686a856f4c871
arkad <mojib.khabireh@gmail.com>
2022-03-31 09:30:13 +0430

    Configuration

---
+++ src/main/java/Entity/Visit.java
@@ -0,0 +1,9 @@
+package Entity;
+
+import java.sql.Time;
+
+public class Visit {
+    private Doctor doctor;
+    private Patient patient;
+    private Time time;
+}
---
+++ src/main/java/MainTest.java
@@ -0,0 +1,9 @@
+import java.sql.Time;
+import java.time.LocalTime;
+
+public class MainTest {
+    public static void main(String[] args) {
+      /*  LocalTime localTime = LocalTime.of(12, 12, 12);
+        System.out.println(localTime);*/
+    }
+}
