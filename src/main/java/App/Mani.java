de9c451805b97cacea36a64a65d8690c4cb555f3
arkad <mojib.khabireh@gmail.com>
2022-03-31 16:32:18 +0430

    menu step 1

--- src/main/java/App/Mani.java
+++ src/main/java/App/Mani.java
@@ -1,8 +1,28 @@
 package App;
 
+import java.util.Scanner;
+
 public class Mani {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Menu menu = new Menu();
+        while (true){
+            try {
+                System.out.println("\t\tfor sing up insert 1\n" +
+                        "for login app insert 2");
+                int se= scanner.nextInt();
+                switch (se){
+                    case 1:
+                        menu.singUp();
+                    case 2:
+                        menu.menu();
+                }
 
+            }catch (Exception e){
+                e.printStackTrace();
+                continue;
+            }
+        }
 
     }
 }
