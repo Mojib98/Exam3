9bde369bcea58de5ce1f74cd3d8b4d2705240e19
arkad <mojib.khabireh@gmail.com>
2022-03-31 12:28:38 +0430

    see All Doctor for patient

--- src/main/java/App/PatientApp.java
+++ src/main/java/App/PatientApp.java
@@ -1,8 +1,10 @@
 package App;
 
+import Entity.Doctor;
 import Entity.Patient;
 import service.imp.PatientService;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -16,6 +18,10 @@ public class PatientApp {
         patient.setName(name);
         patientService.add(patient);
         //exception illegal insert
+    }
+    public void seeAllDoctor(){
+       List<Doctor> list= patientService.allDoctor();
+       list.forEach(System.out::println);
     }
 
 }
