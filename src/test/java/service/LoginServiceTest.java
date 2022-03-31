681c5d262df4afdcfbe26b0101ee5787348be4b8
arkad <mojib.khabireh@gmail.com>
2022-03-31 16:59:18 +0430

    login Test

---
+++ src/test/java/service/LoginServiceTest.java
@@ -0,0 +1,26 @@
+package service;
+
+import Entity.Doctor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginServiceTest {
+    LoginService loginService = new LoginService();
+
+    @Test
+    void isDoctor() {
+        String name ="m32";
+        Integer id = 12;
+       Boolean c= loginService.isDoctor(name,id);
+        assertEquals(false,c);
+    }
+
+    @Test
+    void isPatient() {
+        String name ="m32";
+        Integer id = 12;
+        Boolean c= loginService.isPatient(name,id);
+        assLertEquals(false,c);
+    }
+}
